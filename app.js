e2205623867f7d5928d1a4ebc362558be17174e2
khan8799 <farmankhanmlc@gmail.com>
2022-04-27 20:03:45 +0530

    ap improved

--- app.js
+++ app.js
@@ -1,44 +1,10 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const http = require("http");
+const app = require("./app");
 
-const envPath = ".env.dev";
-dotenv.config({
-  path: envPath,
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
 });
-
-const cors = require("cors");
-
-const db = 'mongodb+srv://twerlo:twerlo@cluster0.q6slj.mongodb.net/sify?retryWrites=true&w=majority';
-// const db = process.env.MONGOHOST;
-mongoose.connect(db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-console.log(db)
-
-const app = express();
-
-app.use(cors());
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json({ limit: "40mb" }));
-
-const userRoutes = require("./routes/user");
-const newsRoutes = require("./routes/news");
-const appointmentRoutes = require("./routes/appointment");
-
-app.use("/api/user", userRoutes);
-app.use("/api/news", newsRoutes);
-app.use("/api/appointment", appointmentRoutes);
-
-app.use((req, res, next) => {
-  const error = new Error("Resource not found");
-  error.status = 404;
-  next(error);
-});
-
-module.exports = app;
-
